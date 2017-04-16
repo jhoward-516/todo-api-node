df8938f73b6a277d4ff2f17799c8ee5439fdb149
John Howard <longhornjoho@gmail.com>
2017-04-16 10:05:04 -0500

    create GET todo route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,7 +1,7 @@
 const expect = require('expect');
 const request = require('supertest');
 
-const {app} = require('./../server')
+const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 beforeEach( (done) => {
