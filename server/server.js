d4c2540b1d9d3b58cab8d52ee7f47fad948563f8
John Howard <longhornjoho@gmail.com>
2017-04-17 23:40:11 -0500

    create GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -10,6 +10,8 @@ const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
 
+const { authenticate } = require('./middleware/authenticate');
+
 let app = express();
 const port = process.env.PORT;
 
@@ -103,6 +105,10 @@ app.post('/users', (req, res) => {
         res.status(400).send(e);
     });
 
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 app.listen(port, () => {
